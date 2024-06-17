33edb937243230ecc04fe8bd294b5688b3af2f3f
Flavia Kosti <flaviakosti@gmail.com>
2024-06-17 16:27:59 -0400

    EasyTask: Use signal inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent { }
+export class AppComponent {
+  users = DUMMY_USERS;
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,9 +8,8 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[0];
+  user = input.required<{ avatar: string, name: string, id: string }>();
+  imagePath = computed(() => `assets/users/${this.user().avatar}`);
 
-  onSelectUser() {
-
-  }
+  onSelectUser() { }
 }
