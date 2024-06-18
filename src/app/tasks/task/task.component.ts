430c8c288121c5dae68b0f3f1a4795ca87ddd6f5
Flavia Kosti <flaviakosti@gmail.com>
2024-06-18 17:53:26 -0400

    EasyTask: Add task component

---
+++ src/app/tasks/task/task.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { Task } from '../dummy-tasks';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  task = input.required<Task>();
+}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,20 @@
 import { Component, input } from '@angular/core';
+import { dummyTasks, Task } from './dummy-tasks';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   user = input.required<any>();
+  tasks: Task[] = dummyTasks;
+  userTasks: Task[] = [];
+
+  ngOnChanges() {
+    this.userTasks = dummyTasks.filter((task: Task) => task.userId === this.user().id);
+  }
 }
