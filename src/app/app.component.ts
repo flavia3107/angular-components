7c9a980338150578946ff089c4e0bb9d916d49d3
Flavia Kosti <flaviakosti@gmail.com>
2024-06-18 17:00:34 -0400

    EasyTask: Create new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, CommonModule],
+  imports: [HeaderComponent, UserComponent, TasksComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  user: { id: string; name: string; avatar: string; } | undefined;
 
   onSelectUser(id: string) {
-    console.log('ID', id)
+    this.user = DUMMY_USERS.find(user => user.id === id);
   }
 }
