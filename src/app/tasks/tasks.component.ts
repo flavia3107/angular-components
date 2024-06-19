ece4a7e2a81c1833092a880c71557be92f16ba2e
Flavia Kosti <flaviakosti@gmail.com>
2024-06-19 15:48:47 -0400

    EasyTask: Managing 'Add new Task'

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { dummyTasks, Task } from './dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -13,12 +14,20 @@ export class TasksComponent {
   user = input.required<any>();
   tasks: Task[] = dummyTasks;
   userTasks: Task[] = [];
-
+  isAddingTaskVisible = false;
   ngOnChanges() {
     this.userTasks = dummyTasks.filter((task: Task) => task.userId === this.user().id);
   }
 
   updateTask(id: string) {
     this.userTasks = this.userTasks.filter(task => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTaskVisible = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTaskVisible = false;
   }
 }
